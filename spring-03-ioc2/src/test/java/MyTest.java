ae73e0179b4d270d7eede4027e34c36fa1678851
hjl19910901 <18710360575@163.com>
2020-02-17 19:42:33 +0800

    Spring

--- spring-03-ioc2/src/test/java/MyTest.java
+++ spring-03-ioc2/src/test/java/MyTest.java
@@ -1,16 +1,14 @@
 import com.hemukeji.pojo.User;
+import com.hemukeji.pojo.UserT;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
 
     public static void main(String[] args) {
-
-
         //在配置文件加载的时候，容器中管理的对象就已经初始化了，Beans就是容器
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-
-       User user = (User) context.getBean("askquestion");
+        UserT user = (UserT) context.getBean("user2");
        user.show();
 
     }
