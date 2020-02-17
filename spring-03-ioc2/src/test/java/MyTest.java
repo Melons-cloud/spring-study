e0a6e8222b8861f921eba561b5573fa832c5c552
hjl19910901 <18710360575@163.com>
2020-02-17 19:53:52 +0800

    Spring

--- spring-03-ioc2/src/test/java/MyTest.java
+++ spring-03-ioc2/src/test/java/MyTest.java
@@ -7,7 +7,7 @@ public class MyTest {
 
     public static void main(String[] args) {
         //在配置文件加载的时候，容器中管理的对象就已经初始化了，Beans就是容器
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserT user = (UserT) context.getBean("user2");
        user.show();
 
